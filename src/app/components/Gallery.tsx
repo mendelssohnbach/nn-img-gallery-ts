5bc2ea148b3c932b247fab6321b38afabda7b8c9
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2023-09-02 15:07:38 +0900

    ✨ #5 - Remote Images

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,3 +1,4 @@
+import ImgContainer from '@/app/components/ImgContainer';
 import fetchImages from '@/lib/fetchImages';
 import type { ImagesResults } from '@/models/Images';
 
@@ -11,10 +12,9 @@ export default async function Gallery() {
   return (
     <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
       {images.photos.map((photo) => (
-        <div
-          key={photo.id}
-          className="h-64 bg-gray-200 rounded-xl"
-        ></div>
+        // Missing "key" prop for element in iteratoreslintreact/jsx-key
+        // eslint-disable-next-line
+        <ImgContainer photo={photo} />
       ))}
     </section>
   );
