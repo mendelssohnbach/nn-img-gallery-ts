1a8095f823a803502c33d60a6c88f229312a35e0
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2023-09-07 16:36:07 +0900

    ✨ #8 - Search Images

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -3,8 +3,14 @@ import fetchImages from '@/lib/fetchImages';
 import type { ImagesResults } from '@/models/Images';
 import addBlurredDataUrls from '@/lib/getBase64';
 
-export default async function Gallery() {
-  const url = 'https://api.pexels.com/v1/curated';
+type Props = {
+  topic?: string | undefined;
+};
+
+export default async function Gallery({ topic }: Props) {
+  const url = !topic
+    ? 'https://api.pexels.com/v1/curated'
+    : `https://api.pexels.com/v1/search?query=${topic}`;
 
   const images: ImagesResults | undefined = await fetchImages(url);
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+import Navbar from '@/app/components/Navbar';
 import './globals.css';
 import type { Metadata } from 'next';
+
+export const revalidate = 3600;
 
 export const metadata: Metadata = {
   title: 'Next.js Image Gallery',
@@ -10,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <main className="max-w-6xl mx-auto">{children}</main>
       </body>
     </html>
